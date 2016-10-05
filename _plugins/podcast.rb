85d8ce562c0fba28473b96d3aa8ceb335ec06afa
Michiaki Ariga <chezou+github@gmail.com>
2016-10-05 21:10:21 +0900

    Add external plugin

--- _plugins/podcast.rb
+++ _plugins/podcast.rb
@@ -4,4 +4,24 @@ module Jekyll
       (input.to_f / 1024 / 1024).round(1)
     end
   end
+
+  class ExtLinkTag < Liquid::Tag
+    @text = ''
+    @link = ''
+
+    def initialize(tag_name, markup, tokens)
+      if markup =~ /(.+)(\s+(https?:\S+))/i
+        @text = $1
+        @link = $3
+      end
+      super
+    end
+
+    def render(context)
+      output = super
+      "<a class='external' target='_blank' href='#{@link}'>#{@text}</a>"
+    end
+  end
 end
+
+Liquid::Template.register_tag('extlink', Jekyll::ExtLinkTag)
